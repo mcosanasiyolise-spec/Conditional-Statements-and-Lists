07e303fe44483bd375b9fb926a7d92321109f4c9
mcosanasiyolise-spec <mcosanasiyolise@gmail.com>
2026-02-04 17:55:57 +0200

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,28 @@
+ import turtle
+
+ turtle.Screen().bgcolor("Green")
+
+ board = turtle.Turtle()
+
+ # Triangle
+  board.foward(100)
+
+  board.left(120)
+  board.foward(100)
+
+  board.left(120)
+  board.foward(100)
+
+
+  #Square
+  board.foward(100)
+  board.left(90)
+
+  board.foward(100)
+  board.left(90)
+
+  board.foward(100)
+  board.left(90)
+
+board.foward(100)
+  board.left(90)
---
+++ mian.py
@@ -0,0 +1,8 @@
+import turtle
+
+turtle.Screen().bgcolor("Orange")
+
+
+sc = turtle.Screen()
+sc.setup(400,300)
+turtle.titel("Welcome to Turtle Window")
